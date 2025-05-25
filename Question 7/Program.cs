579ad5f07ba10ae82f88e0bbd3fdcdbbe8746a62
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-05-25 10:47:27 +0100

    Question 7.csproj

---
+++ Question 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Question_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that finds the greatest of given 5 numbers.
+            int maximum = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($" Enter your number at {i}:  ");
+                int number = int.Parse(Console.ReadLine());
+                if (number > maximum)
+                {
+                    maximum = number;
+                }
+
+            }
+
+            Console.WriteLine($" The greatest number is {maximum}");
+        }
+    }
+}
