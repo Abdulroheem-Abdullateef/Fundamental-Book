b97f10ef64fa9ee5f0e8e8308be92ca907e98c1d
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-05-31 18:09:03 +0100

    Question 1 chapter 6.csproj

---
+++ Chapter 6 Questions/Question 1 chapter6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Question_1_chapter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                    // Write a program that prints on the console the numbers from 1 to N. 
+                    //The number N should be read from the standard input.
+
+            Console.WriteLine("Enter your number ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
