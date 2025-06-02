1d7cc707f1f25d62b9be013b0e89088b0eb2ff7f
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-06-02 14:06:11 +0100

    Question-2-chapter-6.csproj

---
+++ Chapter 6 Questions/Question 2 chapter6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Question_2_chapter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 != 0 || i % 7 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+                
+            
+            }
+
+        }
+    }
+}
