a872b27dd0093101b3af295af3dadc75e135b04a
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-06-03 13:38:58 +0100

    Question-3-chapter-6.csproj

---
+++ Chapter 6 Questions/Question 3 charpter6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Question_3_charpter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                       //3.Write a program that reads from the console a series of integers and
+                         //prints the smallest and largest of them.
+
+            int lowest = 0;
+            int largest = 0;
+            bool firstNumber = true;    
+
+
+            Console.WriteLine("Enter a lenght numbers to know your largest and lowest");
+            int number = int.Parse(Console.ReadLine());
+
+           
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Enter a number");
+                int number2 = int.Parse(Console.ReadLine());
+
+                if (firstNumber)
+                {
+                    largest = number2;
+                    lowest = number2;
+                    firstNumber = false;
+                }
+                else
+                {
+
+                    if (number2 > largest)
+                    {
+                        largest = number2;
+                    }
+
+                    if (number2 < largest)
+                    {
+                        lowest = number2;
+                    }
+
+                }
+
+
+            }
+            Console.WriteLine($" The largest is {largest} and the lowest is {lowest}");
+        }
+    }
+}
