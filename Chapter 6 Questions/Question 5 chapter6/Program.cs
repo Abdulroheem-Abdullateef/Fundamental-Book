61e89718d8180e277727f55dc13582c800fa9082
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-06-10 22:19:09 +0100

    Question-5-chapter-6.csproj

---
+++ Chapter 6 Questions/Question 5 chapter6/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Question_5_chapter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+                //  Fibonacci numbers start from 0 and 1, each additional is obtained as
+                //the sum of the previous two.You can find the first n Fibonacci
+                //numbers with a for-loop from 1 to n, and at each iteration calculate the
+                //next number by using the previous two(which you will keep in two
+                //additional variables).
+                //6.Multiply the number
+
+
+
+           double value1 = 0;
+            double value2 = 1;
+            
+
+
+            Console.WriteLine("r1");
+
+            Console.WriteLine("r2"); 
+
+            Console.WriteLine("Enter your number");
+            double n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double value3 = value1 + value2;
+
+                value1 = value2;
+                value2 = value3;
+
+
+                Console.WriteLine(value3);
+
+            }
+
+
+        }
+    }
+}
