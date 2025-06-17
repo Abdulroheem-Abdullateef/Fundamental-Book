24f0d137bd84cea830114f04c13a9bde09c9752d
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-06-17 23:10:02 +0100

    Question-7-chapter-6.csproj

---
+++ Chapter 6 Questions/Question 7 chapter6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Question_7_chapter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                    //  Write a program that calculates N!*K! / (N - K)! for given N and K
+                    //(1 < K < N).
+
+            Console.WriteLine("Enter first number for factorial N ");
+            ulong factorialN = ulong.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter first number for factorial K ");
+            ulong factorialK = ulong.Parse(Console.ReadLine());
+
+            ulong difference = factorialN - factorialK;
+
+
+            for (ulong i = factorialN - 1; i > 0; i--)
+            {
+                factorialN *= i;
+            }
+
+            for (ulong i = factorialK - 1; i > 0; i--)
+            {
+                factorialK *= i;
+            }
+                
+            
+            for (ulong i = difference - 1; i > 0; i--)
+            {
+                difference *= i;
+            
+            }
+            Console.WriteLine("Result is {0}", (factorialN * factorialK) / difference);
+
+        }
+    }
+}
