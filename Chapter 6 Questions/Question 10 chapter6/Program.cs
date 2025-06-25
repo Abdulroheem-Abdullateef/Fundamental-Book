2ff0cc4cdff525f9e666804ca04a8419d2607aa0
Abdulrohee-Abdullateef <roheemlateef.3@gmail.com>
2025-06-25 17:29:01 +0100

    Question-10-chapter-6.csproj

---
+++ Chapter 6 Questions/Question 10 chapter6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Question_10_chapter6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter  a number for triangle");
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = i; j < i + number; j++)
+                {
+                    Console.Write(j + "  ");
+                    
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+
+            
+
+        }
+    }
+}
